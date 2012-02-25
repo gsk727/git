2d4732a6085a08c61fc65c4a7e18a5e3c850e356
shukai.guo <gsk727@gmail.com>
2012-02-25 08:25:26 -0500

    add flask

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 #-*- coding: utf-8 -*-
 # new branch
+from flask import Flask, Blueprint
+
+v = Blueprint("v", __name__, static_folder="static")
+@v.route("/")
+def index():
+	return "hello world"
+
+app = Flask(__name__)
+app.register_blueprint(v)
+app.run(host="0.0.0.0", port = 80)
+
 if __name__ == "__main__":
 	print "only for test"
 
