83d4ebee36ade70931225fcd8ae9e8a47f1a4d18
root <root@gsk.(none)>
2012-03-10 15:04:26 +0800

    modify

--- prepare/views/db.py
+++ prepare/views/db.py
@@ -2,7 +2,7 @@
 import pymongo
 
 try:
-    _Conn = pymongo.Connection("192.168.1.109", 27017)
+    _Conn = pymongo.Connection("127.0.0.1", 27017)
     DB = _Conn.reader
 except pymongo.errors.AutoReconnect, des:
     #_Conn.close()
