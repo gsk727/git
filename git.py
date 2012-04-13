aad241f78c572f34d76484c1a1c940bc556d6078
root <root@gsk.(none)>
2012-04-13 15:24:32 +0800

    1212121

--- git.py
+++ git.py
@@ -1,9 +1,11 @@
 import os
 for i in xrange(100):
-    with open(str(i), "w+") as f:
-        f.write(str(i))
-    
-    os.system("git add .")
-    os.system("git commit -m %d"%(i,))
-    os.system("git push")
+    os.system("rm -rf %d"%(i,))
+
+   # with open(str(i), "w+") as f:
+   #     f.write(str(i))
+        
+#    os.system("git add .")
+#    os.system("git commit -m %d"%(i,))
+#    os.system("git push")
 
